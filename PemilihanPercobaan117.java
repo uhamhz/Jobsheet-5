5fed2fe005efb93f0b453795657159ff0f99d79b
uhamhz <ammarhafizh2404@gmail.com>
2023-09-25 12:08:25 +0700

    added ternary operator

--- PemilihanPercobaan117.java
+++ PemilihanPercobaan117.java
@@ -1,17 +1,19 @@
 import java.util.Scanner;
+
 /**
  * PemilihanPercobaan117
  */
 public class PemilihanPercobaan117 {
     public static void main(String[] args) {
         Scanner input00 = new Scanner(System.in);
+        
         int angka;
+        String hasil;
+
         System.out.print("Masukan angka : ");
         angka = input00.nextInt();
-        if (angka % 2 ==0) {
-            System.out.println("Angka "+angka+" Bilangan Genap");
-        }else{
-            System.out.println("Angka "+angka+" Bilangan Ganjil");
-        }
+
+        hasil = (angka % 2 == 0) ? " Bilangan Genap" : " Bilangan Ganjil";
+        System.out.println("Angka "+angka+ hasil);
     }
 }
