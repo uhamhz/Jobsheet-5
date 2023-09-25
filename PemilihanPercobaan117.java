5258c8ef25fd78d858f2fe30186c6e83e8a60ad7
uhamhz <ammarhafizh2404@gmail.com>
2023-09-25 21:08:23 +0700

    Changes Scanner Input

--- PemilihanPercobaan117.java
+++ PemilihanPercobaan117.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
  */
 public class PemilihanPercobaan117 {
     public static void main(String[] args) {
-        Scanner input00 = new Scanner(System.in);
+        Scanner input17 = new Scanner(System.in);
         
         int angka;
         String hasil;
 
         System.out.print("Masukan angka : ");
-        angka = input00.nextInt();
+        angka = input17.nextInt();
 
         hasil = (angka % 2 == 0) ? " Bilangan Genap" : " Bilangan Ganjil";
         System.out.println("Angka "+angka+ hasil);
--- PemilihanPercobaan217.java
+++ PemilihanPercobaan217.java
@@ -2,17 +2,17 @@ import java.util.Scanner;
 
 public class PemilihanPercobaan217 {
     public static void main(String[] args) {
-        Scanner input00 = new Scanner(System.in);
+        Scanner input17 = new Scanner(System.in);
         float uas,uts,kuis,tugas,total;
         
         System.out.println("Nilai UAS   :");
-        uas = input00.nextFloat();
+        uas = input17.nextFloat();
         System.out.println("Nilai UTS   :");
-        uts = input00.nextFloat();
+        uts = input17.nextFloat();
         System.out.println("Nilai Kuis  :");
-        kuis = input00.nextFloat();
+        kuis = input17.nextFloat();
         System.out.println("Nilai Tugas :");
-        tugas = input00.nextFloat();
+        tugas = input17.nextFloat();
 
         total = (uas * 0.4f) + (uts * 0.3f) + (kuis * 0.1f) + (tugas * 0.2f);
 
