c02c50d973ac7b8bf27b6a81aec903787871a38f
uhamhz <ammarhafizh2404@gmail.com>
2023-09-27 19:56:57 +0700

    added range max and min in else if

--- PemilihanPercobaan217.java
+++ PemilihanPercobaan217.java
@@ -16,19 +16,26 @@ public class PemilihanPercobaan217 {
 
         total = (uas * 0.4f) + (uts * 0.3f) + (kuis * 0.1f) + (tugas * 0.2f);
 
-        String Message = total < 65 ? "Remidi" : "Tidak Remidi";
+        // if (total > 100) {
+        //     System.out.println("Nilai yang Anda Masukan Salah");
+        // } else if (total >= 0 && total <= 100){
+        //     String Message = total < 65 ? "Remidi" : "Tidak Remidi";
+        // }
 
-        if (total > 80) {
+        String Message = total < 65 ? "Remidi" : "Tidak Remidi";
+        
+
+        if (total >= 81 && total <= 100) {
             System.out.println("Anda Mendapatkan Nilai A Kualifikasi Sangat Baik");
-        } else if (total > 73) {
+        } else if (total > 73 && total <= 80) {
             System.out.println("Anda Mendapatkan Nilai B+ Kualifikasi Lebih Dari Baik");
-        } else if (total > 65){
+        } else if (total > 65 && total <= 73){
             System.out.println("Anda Mendapatkan Nilai B Kualifikasi Baik");
-        } else if (total > 60){
+        } else if (total > 60 && total <= 65){
             System.out.println("Anda Mendapatkan Nilai C+ Kualifikasi Lebih Dari Cukup");
-        } else if (total > 50){
+        } else if (total > 50 && total <= 60){
             System.out.println("Anda Mendapatkan Nilai C Kualifikasi Cukup ");
-        } else if (total > 39){
+        } else if (total > 39 && total <= 50){
             System.out.println("Anda Mendapatkan Nilai D Kualifikasi Kurang");
         } else{
             System.out.println("Anda Mendapatkan Nilai E Kualifikasi Gagal");
